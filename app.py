53c8fb9c1f979349a47acd51d1adddf0874348e3
cletus <cletusk8@gmail.com>
2023-02-03 15:20:45 -0500

    commit 3 feb

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-print("Hola Mundo")
-suma = int(input("Digite su edad: "))
-if suma >= 0 and suma <= 150:
-    print("Su edad es: " , suma)
-else:
-    print("Es increible que tenga mas")
+print("BIENVENIDO")
+edad = int(input("Digite su edad: "))
+if edad >= 0 and edad <=150:
+    print("Su edad es: " , edad)
+elif edad < 0 or edad > 150:
+    print("Es increible que tenga" , edad , "y este vivo")
